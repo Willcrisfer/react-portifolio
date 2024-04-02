97a866b812270245811faff21c316bf7d214c430
Willian Fernandess <wFernandes@Smartwatt.local>
2024-04-02 15:23:24 +0100

    Updates

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -12,19 +12,19 @@ function HomePage() {
  const { profile }  = useProfile();
 
     return (
-        <header id="header" className="max-width">
-            <div className="header__left">
+        <section id="home" className="max-width">
+            <div className="home__left">
                 <h1>{profile?.name}</h1>
                 <p>{profile?.homeDescription}</p>
                 <NavLink to="/about"><button className="btn btn-primary" onClick={scrollToTop} >Sobre mim</button></NavLink>
             </div>
-            <div className="header__right">
-                <div className="header__image">
+            <div className="home__right">
+                <div className="home__image">
                     <img src={imgprofile} alt="Willian Fernandes" className="profile__img" />
-                    <img src={abstract} alt="background" className="background" />
+                    <img src={abstract} alt="background" className="home__background" />
                 </div>
             </div>
-        </header>
+        </section>
     );
 };
 
