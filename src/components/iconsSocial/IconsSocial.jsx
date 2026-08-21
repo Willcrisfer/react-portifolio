f2c1826f3bb48f869b71acbbb14952c7c53a875d
Willcrisfer <127127402+Willcrisfer@users.noreply.github.com>
2026-08-21 17:20:08 +0100

    New_instagram

--- src/components/iconsSocial/IconsSocial.jsx
+++ src/components/iconsSocial/IconsSocial.jsx
@@ -1,7 +1,7 @@
 import { FaInstagram, FaYoutube } from 'react-icons/fa';
 const socialNetworks = [
     { name: "youtube", icon: <FaYoutube  />, link:"https://www.youtube.com/channel/UCahTcai1vzmzbiuHLPhRFYg"},
-    { name: "instagram", icon: <FaInstagram />, link:"https://www.instagram.com/will_crisfer/"},
+    { name: "instagram", icon: <FaInstagram />, link:"https://www.instagram.com/willianfernandes.music/"},
   ]
 
 
